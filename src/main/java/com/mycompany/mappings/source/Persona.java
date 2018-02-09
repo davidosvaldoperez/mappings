ea26b6fedd290fbcdaabc393b3aed1af3e4a32df
David <david.perez@hospitalitaliano.org.ar>
2018-02-09 16:57:21 -0300

    primer modelo ejemplo

---
+++ src/main/java/com/mycompany/mappings/source/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mappings.source;
+
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @author nefre
+ */
+@Data
+public class Persona {
+    String tipoDoc;
+    Integer nroDoc;
+    String nombre;
+    String apellido;
+    Date fechaNac;
+}
